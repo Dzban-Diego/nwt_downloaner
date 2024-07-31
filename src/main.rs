93d9075d2d1575380f8e55f5cbcfaf34bdc71cf2
Klimczewski <62210828+Dzban-Diego@users.noreply.github.com>
2024-07-31 13:09:26 +0200

    Nie dziła Projekt odrzucony

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,32 @@ async fn main() {
 }
 
 async fn get_data() {
-    let url = format!("https://www.jw.org/pl/biblioteka/biblia/biblia-wydanie-do-studium/ksiegi/json/html/42001024-42080051",);
+    let _url_test = format!("https://b.jw-cdn.org/apis/pub-media/GETPUBMEDIALINKS?output=json&pub=osg&fileformat=MP3%2CAAC&alllangs=0&langwritten=PL&txtCMSLang=PL");
+
+    let range = "01001003";
+    let url = format!(
+        "https://www.jw.org/pl/biblioteka/biblia/biblia-wydanie-do-studium/ksiegi/json/html/{}",
+        &range
+    );
+
+    let test_url = "https://jsonplaceholder.typicode.com/todos/1";
 
     println!("test");
-    let res = reqwest::get(url)
-        .await
-        .expect("o co tu chodzi???")
-        .json::<serde_type::Root>()
-        .await
-        .expect("o co tu chodzi2???");
+    dbg!(&url);
+    let res = reqwest::get(test_url).await.unwrap();
 
-    let data = serde_json::to_string(&res).expect("no musi to być");
-    println!("test kurde {}", data);
+    // .json::<serde_type::Book>()
+    // .await;
+    dbg!(&res);
 
     println!("test kurde");
+    // match res {
+    //     Ok(..) => {
+    //         println!("res OK");
+    //     }
+    //     Err(e) => {
+    //         println!("Nie no kupa");
+    //         panic!("Nie udało się pobrać piosenek. Err: {}", e)
+    //     }
+    // }
 }
